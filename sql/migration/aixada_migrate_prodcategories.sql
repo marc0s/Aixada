3204760fb84dbebf9023398b62a48a6b091a5339
marc0s <marcos@tenak.net>
2013-04-03 16:33:14 +0200

    Fitxer per actualitzar BD a socarrel
    SQL per executar a bases de dades d'una altra branca que
    no té aquestes categories.

---
+++ sql/migration/aixada_migrate_prodcategories.sql
@@ -0,0 +1,7 @@
+-- added categories for socarrel
+insert into aixada_product_category values (1500, 'prdcat_legumes');
+insert into aixada_product_category values (1600, 'prdcat_eatseeds');
+insert into aixada_product_category values (14100, 'prdcat_sauces');
+insert into aixada_product_category values (14200, 'prdcat_oilandvinegar');
+insert into aixada_product_category values (19100, 'prdcat_driedfruits');
+
