9b0551835dea4501f0407172cd5ead6b07e1a8cf
marc0s <marcos@tenak.net>
2013-05-23 00:47:41 +0200

    Allow modifying zero-quantity orders
    Allow to modify the quantities of products for UF who haven't
    ordered at first showing the hidden cells and adding 0 as
    ordered quantity.

--- manage_orders.php
+++ manage_orders.php
@@ -237,7 +237,16 @@
 
 						});
 
-						
+                        $('th.hidden').removeClass('hidden').show();
+
+                        $('.hidden.interactiveCell').each(function() {
+                            var row = $(this).attr('row');
+                            var col = $(this).attr('col');
+                            var pid = row + '_' + col ;
+                            $(this).append('<p id="'+pid+'" class="textAlignCenter editable">0</p>');
+                            $(this).removeClass('hidden').show();
+                        });
+
 						var total = "<span>"+quTotal.toFixed(2)+"</span><span class='shopQuantity'>("+quShopTotal+")</span>";
 						$('.total_'+lastId).html(total);
 
